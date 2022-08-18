f003733b739830e5325c3ed8709c879889df40d9
Clousbi <biancamthope@gmail.com>
2022-08-18 10:15:39 -0300

    main

---
+++ NumeroPrimoWhile/NumeroPrimoWhile/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroPrimoWhile
+{
+    internal class Program
+    {
+        static bool NumeroPrimo (int n)
+        {
+            if ((n == 0) || (n == 1))
+            {
+                return (false);
+            }
+
+            if (n == 2)
+            {
+                return (true);
+            }
+
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return (false);
+        }
+        static void Main(string[] args)
+        {
+            int n,resp;
+            
+            do
+            {
+                Console.WriteLine("Digite um número:");
+                n = int.Parse(Console.ReadLine());
+
+                if (NumeroPrimo(n) == true)
+                {
+                    Console.WriteLine("O numero é primo");
+
+                }
+                else
+                {
+                    Console.WriteLine("O número não pe primo");
+                }
+
+                Console.WriteLine("Deseja digitar outro número? ('1' para sim, '0' para não)");
+                resp = int.Parse(Console.ReadLine());
+
+            } while (resp != 0);
+        }
+    }
+}
---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string Nome;
+            float Salário;
+            float NovoSalário;
+            float Percentual;
+
+            Console.Clear();        //Limpa a Tela
+
+            Console.Write("Nome do Funcionário..: ");
+            Nome = Console.ReadLine();
+
+            Console.Write("Salário..............: ");
+            Salário = float.Parse(Console.ReadLine());
+
+            Console.Write("Aumento Percentual...: ");
+            Percentual = float.Parse(Console.ReadLine());
+
+            NovoSalário = Salário + Salário * Percentual / 100;
+
+            Console.WriteLine("\nO(A) Funcionário(a) {0}, que recebia {1:F2}, passará a receber {2:F2}", Nome, Salário, NovoSalário);
+            Console.WriteLine("já que teve um aumento de {0:F1} %.\n\n", Percentual);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
