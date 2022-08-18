abf45e36ec618d321a1070318a615b5ae32f851b
Clousbi <biancamthope@gmail.com>
2022-08-18 19:23:36 -0300

    main

---
+++ Base2base10/Exercicio1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string binario;
+            double base10 = 0, exp = 0;
+
+
+            Console.WriteLine("Digite o número binário:");
+            binario = Console.ReadLine();
+
+            for (int i = binario.Length-1; i >= 0 ; i--)
+            {
+                base10 = base10 + char.GetNumericValue(binario[i]) * Math.Pow(2, exp);
+                exp++;
+            }
+            Console.WriteLine($"Esse número na base 10 é {base10}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SomaRaiz/SomaRaiz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomaRaiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int P1, P2, Soma, Quadrado;
+            for (int i = 1000; i <= 9999; i++)
+            {
+                P1 = i / 100;
+                P2 = i % 100;
+
+                Soma = P1 + P2;
+                Quadrado = Soma * Soma;
+
+                if (Quadrado == i)
+                {
+                    Console.WriteLine($"O número {i} possui essa característica...");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
