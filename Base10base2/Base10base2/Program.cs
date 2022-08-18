59a2da1c9638496e8741713024324e6b66471970
Clousbi <biancamthope@gmail.com>
2022-08-18 11:10:15 -0300

    main

---
+++ Base10base2/Base10base2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base10base2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, n2;
+
+            Console.WriteLine("Digite um número:");
+            n= int.Parse(Console.ReadLine());
+
+            i = n;
+
+            while(i != 0)
+            {
+                n2 = n % 2;
+                n = n / 2;
+               
+                Console.Write($"{n2},");
+                
+                i--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NumeroPerfeito/NumeroPerfeito/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroPerfeito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int divisores;
+
+            for (int i = 0; i <= 1000; i++)
+            {
+                divisores = 0;
+
+                for (int j = 1; j < i; j++)
+                {
+
+                    if (i % j == 0)
+                    {
+                        divisores = divisores + j;
+                    }
+                    
+                }
+                if (divisores == i)
+                {
+                    Console.WriteLine($"O número {i} é um número perfeito");
+                }
+
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
