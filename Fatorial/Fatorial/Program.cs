dcb28a7881344c714cccba428646f782b6db0642
Clousbi <biancamthope@gmail.com>
2022-08-25 10:37:27 -0300

    main

---
+++ Fatorial/Fatorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatorial
+{
+    internal class Program
+    {
+        static int Fatorial(int x)
+        {
+            int fatn = 1;
+            for (int i = 1; i <= x; i++)
+            {
+                fatn = fatn * i;
+            }
+            return (fatn);
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Digite o número:");
+            n = int.Parse(Console.ReadLine());
+            Console.Write($"O fatorial de {n} é {Fatorial(n)}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PotenciaReferencia/PotenciaReferencia/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotenciaReferencia
+{
+    internal class Program
+    {
+        static double Potencia(ref double x, ref double y)
+        {
+            double potencia;
+            potencia = Math.Pow(x, y);
+            return (potencia);
+        }
+
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.Write("Digite o número da base:");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Digite o número do expoente:");
+            y = double.Parse(Console.ReadLine());
+            Console.Write($"A potência de {y} na base {x} é {Potencia(ref x, ref y)}");
+
+            Console.ReadKey();
+        }
+    }
+}
